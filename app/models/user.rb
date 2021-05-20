365637ec99229071818f038a6e14a974dc2e077b
Imran <imranr2@msn.com>
2021-05-20 10:55:57 +0800

    Added api files to nuspm-api

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i 
+
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false,
+        message: -> (object, data) do
+            "#{data[:value]} exists. Please login instead."
+        end
+    }
+    validates :password, length: { minimum:8 }
+    validates :password_confirmation, presence: true, length: { minimum:8 }, on: :create, on: :update 
+end
