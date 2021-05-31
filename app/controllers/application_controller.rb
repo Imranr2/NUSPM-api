70f37a67363ee124630a6717b16e725dda526c89
Imran <imranr2@msn.com>
2021-05-31 10:45:53 +0800

    Skip authentication when loading index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
     before_action :authenticate_request
+    skip_before_action :authenticate_request, :only => [:index]
     attr_reader :current_user
 
     def index
