d1e8bb9d93154a798186b5d0175188f352b5dc2f
Imran <imranr2@msn.com>
2021-06-19 14:48:31 +0800

    revert changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::API
 
     def authenticate_request
         @current_user = AuthorizeApiRequest.call(request.headers).result
-        # render json: { error: 'Not Authorized' }, status: 401 unless @current_user
-        render json: @current_user.errors.full_messages, status: 401 unless @current_user
+        render json: { error: 'Not Authorized' }, status: 401 unless @current_user
     end
 end
