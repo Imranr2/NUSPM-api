1e7653356a48570ff881b8512ad35210a3b3e1a9
Imran <imranr2@msn.com>
2021-08-05 21:01:15 +0800

    updated password regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ApplicationRecord
 
     has_many :notifications, dependent: :destroy
 
-    # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     VALID_EMAIL_REGEX = /e\d{7}@u.nus.edu/i
+    PASSWORD_REGEX = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x
 
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false,
         message: -> (object, data) do
             "#{data[:value]} exists. Please login instead."
         end
     }
-    validates :password, length: { minimum:8 }
+    validates :password, presence: true, format: { with: PASSWORD_REGEX, message: -> (object, data) do 
+        "needs to contain at least 1 uppercase character, 1 lowercase character, 1 digit and a symbol"
+    end }, length: { minimum:8 }
     validates :password_confirmation, presence: true, length: { minimum:8 }, on: :create, on: :update 
 end
